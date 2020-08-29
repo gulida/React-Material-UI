6fb3d4bcb1f7420bd49db585a0e5372dbc22e45d
Kimsanova <synak2013@gmail.com>
2020-08-29 13:03:17 +0600

    Updates

---
+++ precache-manifest.c1de969724dd86e94566a7727e4dbf28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ca93b720b74a0090bc4de7997369d1",
+    "url": "/React-Material-UI/index.html"
+  },
+  {
+    "revision": "fce5af00c031d707172c",
+    "url": "/React-Material-UI/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e67ec8bccdea8326f35",
+    "url": "/React-Material-UI/static/js/2.1d9c8485.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Material-UI/static/js/2.1d9c8485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce5af00c031d707172c",
+    "url": "/React-Material-UI/static/js/main.9aef4ecf.chunk.js"
+  },
+  {
+    "revision": "56c21b693e09563356c0",
+    "url": "/React-Material-UI/static/js/runtime-main.ecb5d878.js"
+  }
+]);
